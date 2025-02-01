d7f70b9daff374b612408cfea7839f3464bcf710
S0URABH_choudhary <subh7898765@gmail.com>
2025-02-01 10:23:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage.jsx";
-import Questionspage from "./components/questionspage.jsx";
+import Questionspage from "./components/Questionspage.jsx";
 
 function App() {
   return (
